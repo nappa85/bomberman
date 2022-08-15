b36201384baceb2ab4109b737ce0d44cabd93a35
Marco Napetti <nappa85@therocktrading.com>
2022-08-15 15:21:18 +0200

    Add walls and game overr

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use bevy::{
     sprite::collide_aabb::{collide, Collision},
 };
 
+const WALL_THICKNESS: f32 = 10.0;
 // x coordinates
 const RIGHT_WALL: f32 = BRICK_SIZE.x * (COLS as f32) / 2.;
 const LEFT_WALL: f32 = -RIGHT_WALL;
@@ -14,16 +15,18 @@ const BOTTOM_WALL: f32 = -TOP_WALL;
 
 const BRICK_SIZE: Vec2 = Vec2::new(50., 50.);
 const BOMB_SIZE: Vec2 = Vec2::new(40., 40.);
+const PLAYER_SIZE: Vec2 = Vec2::new(40., 40.);
 
 const MOVE_SPEED_X: f32 = BRICK_SIZE.x / 10.;
 const MOVE_SPEED_Y: f32 = BRICK_SIZE.y / 10.;
 
 const SCOREBOARD_FONT_SIZE: f32 = 40.0;
 const SCOREBOARD_TEXT_PADDING: Val = Val::Px(5.0);
+const GAMEOVER_FONT_SIZE: f32 = 400.0;
 
 const BACKGROUND_COLOR: Color = Color::rgb(0.9, 0.9, 0.9);
 const PLAYER_COLOR: Color = Color::rgb(0.3, 0.3, 0.7);
-const BRICK_COLOR: Color = Color::rgb(0.5, 0.5, 1.0);
+const BRICK_COLOR: Color = Color::rgb(0.4, 0.0, 0.0);
 const WALL_COLOR: Color = Color::rgb(0.8, 0.8, 0.8);
 const TEXT_COLOR: Color = Color::rgb(0.5, 0.5, 1.0);
 const SCORE_COLOR: Color = Color::rgb(1.0, 0.5, 0.5);
@@ -37,7 +40,7 @@ const COLS: usize = 13;
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .insert_resource(Scoreboard { score: 0 })
+        .insert_resource(Scoreboard::default())
         .insert_resource(ClearColor(BACKGROUND_COLOR))
         .add_startup_system(setup)
         .add_event::<ExplosionEvent>()
@@ -102,6 +105,77 @@ struct Scoreboard {
     score: usize,
 }
 
+// This bundle is a collection of the components that define a "wall" in our game
+#[derive(Bundle)]
+struct WallBundle {
+    // You can nest bundles inside of other bundles like this
+    // Allowing you to compose their functionality
+    #[bundle]
+    sprite_bundle: SpriteBundle,
+}
+
+/// Which side of the arena is this wall located on?
+enum WallLocation {
+    Left,
+    Right,
+    Bottom,
+    Top,
+}
+
+impl WallLocation {
+    fn position(&self) -> Vec2 {
+        match self {
+            WallLocation::Left => Vec2::new(LEFT_WALL - WALL_THICKNESS / 2., 0.),
+            WallLocation::Right => Vec2::new(RIGHT_WALL + WALL_THICKNESS / 2., 0.),
+            WallLocation::Bottom => Vec2::new(0., BOTTOM_WALL - WALL_THICKNESS / 2.),
+            WallLocation::Top => Vec2::new(0., TOP_WALL + WALL_THICKNESS / 2.),
+        }
+    }
+
+    fn size(&self) -> Vec2 {
+        let arena_height = TOP_WALL - BOTTOM_WALL;
+        let arena_width = RIGHT_WALL - LEFT_WALL;
+        // Make sure we haven't messed up our constants
+        assert!(arena_height > 0.0);
+        assert!(arena_width > 0.0);
+
+        match self {
+            WallLocation::Left | WallLocation::Right => {
+                Vec2::new(WALL_THICKNESS, arena_height + WALL_THICKNESS)
+            }
+            WallLocation::Bottom | WallLocation::Top => {
+                Vec2::new(arena_width + WALL_THICKNESS, WALL_THICKNESS)
+            }
+        }
+    }
+}
+
+impl WallBundle {
+    // This "builder method" allows us to reuse logic across our wall entities,
+    // making our code easier to read and less prone to bugs when we change the logic
+    fn new(location: WallLocation) -> WallBundle {
+        WallBundle {
+            sprite_bundle: SpriteBundle {
+                transform: Transform {
+                    // We need to convert our Vec2 into a Vec3, by giving it a z-coordinate
+                    // This is used to determine the order of our sprites
+                    translation: location.position().extend(0.0),
+                    // The z-scale of 2D objects must always be 1.0,
+                    // or their ordering will be affected in surprising ways.
+                    // See https://github.com/bevyengine/bevy/issues/4149
+                    scale: location.size().extend(1.0),
+                    ..default()
+                },
+                sprite: Sprite {
+                    color: WALL_COLOR,
+                    ..default()
+                },
+                ..default()
+            },
+        }
+    }
+}
+
 // Add the game's entities to our world
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     // Camera
@@ -114,7 +188,7 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
             transform: Transform {
                 // TODO: define starting point
                 translation: Vec3::new(LEFT_WALL + BRICK_SIZE.x / 2., TOP_WALL - BRICK_SIZE.y / 2., 0.0),
-                scale: BRICK_SIZE.extend(0.0),
+                scale: PLAYER_SIZE.extend(0.0),
                 ..default()
             },
             sprite: Sprite {
@@ -152,6 +226,12 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
         }),
     );
 
+    // Walls
+    commands.spawn_bundle(WallBundle::new(WallLocation::Left));
+    commands.spawn_bundle(WallBundle::new(WallLocation::Right));
+    commands.spawn_bundle(WallBundle::new(WallLocation::Bottom));
+    commands.spawn_bundle(WallBundle::new(WallLocation::Top));
+
     // In Bevy, the `translation` of an entity describes the center point,
     // not its bottom-left corner
     let offset_x = LEFT_WALL + BRICK_SIZE.x / 2.;
@@ -171,7 +251,7 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
                     .insert(Brick)
                     .insert_bundle(SpriteBundle {
                         sprite: Sprite {
-                            color: BRICK_COLOR,
+                            color: WALL_COLOR,
                             ..default()
                         },
                         transform: Transform {
@@ -190,7 +270,7 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
                     .insert(Brick)
                     .insert_bundle(SpriteBundle {
                         sprite: Sprite {
-                            color: WALL_COLOR,
+                            color: BRICK_COLOR,
                             ..default()
                         },
                         transform: Transform {
@@ -310,6 +390,7 @@ fn check_for_explosions(
 
 fn explode(
     mut commands: Commands,
+    asset_server: Res<AssetServer>,
     mut scoreboard: ResMut<Scoreboard>,
     bomb_collision_query: Query<(Entity, &Bomb, &Transform), (Without<Brick>, Without<Player>, With<Bomb>)>,
     brick_collision_query: Query<(Entity, &Transform), (With<Brick>, With<Breakable>, Without<Player>, Without<Bomb>)>,
@@ -328,11 +409,9 @@ fn explode(
                 }
 
                 // horizontal
-                if collide(bomb_transform.translation, Vec2::new(BRICK_SIZE.x * (2. * (bomb.power as f32) + 1.), BRICK_SIZE.y), other_bomb_transform.translation, other_bomb_transform.scale.truncate()).is_some() {
-                    event_writer.send(Explosion2Event(other_bomb_entity));
-                }
+                if collide(bomb_transform.translation, Vec2::new(BRICK_SIZE.x * (2. * (bomb.power as f32) + 1.), BRICK_SIZE.y), other_bomb_transform.translation, other_bomb_transform.scale.truncate()).is_some()
                 // vertical
-                else if collide(bomb_transform.translation, Vec2::new(BRICK_SIZE.x, BRICK_SIZE.y * (2. * (bomb.power as f32) + 1.)), other_bomb_transform.translation, other_bomb_transform.scale.truncate()).is_some() {
+                || collide(bomb_transform.translation, Vec2::new(BRICK_SIZE.x, BRICK_SIZE.y * (2. * (bomb.power as f32) + 1.)), other_bomb_transform.translation, other_bomb_transform.scale.truncate()).is_some() {
                     event_writer.send(Explosion2Event(other_bomb_entity));
                 }
             }
@@ -340,12 +419,9 @@ fn explode(
             // brick
             for (brick_entity, brick_transform) in &brick_collision_query {
                 // horizontal
-                if collide(bomb_transform.translation, Vec2::new(BRICK_SIZE.x * (2. * (bomb.power as f32) + 1.), BRICK_SIZE.y), brick_transform.translation, brick_transform.scale.truncate()).is_some() {
-                    scoreboard.score += 1;
-                    commands.entity(brick_entity).despawn();
-                }
+                if collide(bomb_transform.translation, Vec2::new(BRICK_SIZE.x * (2. * (bomb.power as f32) + 1.), BRICK_SIZE.y), brick_transform.translation, brick_transform.scale.truncate()).is_some()
                 // vertical
-                else if collide(bomb_transform.translation, Vec2::new(BRICK_SIZE.x, BRICK_SIZE.y * (2. * (bomb.power as f32) + 1.)), brick_transform.translation, brick_transform.scale.truncate()).is_some() {
+                || collide(bomb_transform.translation, Vec2::new(BRICK_SIZE.x, BRICK_SIZE.y * (2. * (bomb.power as f32) + 1.)), brick_transform.translation, brick_transform.scale.truncate()).is_some() {
                     scoreboard.score += 1;
                     commands.entity(brick_entity).despawn();
                 }
@@ -358,13 +434,15 @@ fn explode(
                 }
 
                 // horizontal
-                if collide(bomb_transform.translation, Vec2::new(BRICK_SIZE.x * (2. * (bomb.power as f32) + 1.), BRICK_SIZE.y), player_transform.translation, player_transform.scale.truncate()).is_some() {
-                    scoreboard.score += 100;
-                    commands.entity(player_entity).despawn();
-                }
+                if collide(bomb_transform.translation, Vec2::new(BRICK_SIZE.x * (2. * (bomb.power as f32) + 1.), BRICK_SIZE.y), player_transform.translation, player_transform.scale.truncate()).is_some()
                 // vertical
-                else if collide(bomb_transform.translation, Vec2::new(BRICK_SIZE.x, BRICK_SIZE.y * (2. * (bomb.power as f32) + 1.)), player_transform.translation, player_transform.scale.truncate()).is_some() {
-                    scoreboard.score += 1;
+                || collide(bomb_transform.translation, Vec2::new(BRICK_SIZE.x, BRICK_SIZE.y * (2. * (bomb.power as f32) + 1.)), player_transform.translation, player_transform.scale.truncate()).is_some() {
+                    if player_entity == bomb.player {
+                        game_over(&mut commands, &asset_server);
+                    }
+                    else {
+                        scoreboard.score += 100;
+                    }
                     commands.entity(player_entity).despawn();
                 }
             }
@@ -430,6 +508,45 @@ fn explode2(
 }
 
 fn update_scoreboard(scoreboard: Res<Scoreboard>, mut query: Query<&mut Text>) {
-    let mut text = query.single_mut();
-    text.sections[1].value = scoreboard.score.to_string();
+    if let Ok(mut text) = query.get_single_mut() {
+        text.sections[1].value = scoreboard.score.to_string();
+    }
+}
+
+fn game_over(commands: &mut Commands, asset_server: &AssetServer) {
+    commands.spawn()
+        .insert_bundle(NodeBundle {
+            style: Style {
+                size: Size::new(Val::Percent(100.0), Val::Percent(100.0)),
+                position_type: PositionType::Absolute,
+                justify_content: JustifyContent::Center,
+                align_items: AlignItems::FlexEnd,
+                ..Default::default()
+            },
+            // material: materials.add(Color::NONE.into()),
+            ..Default::default()
+        })
+        .insert_bundle(TextBundle {
+            text: Text {
+                sections: vec![TextSection {
+                    value: "GAME\nOVER".to_string(),
+                    style: TextStyle {
+                        font: asset_server.load("fonts/FiraSans-Bold.ttf"),
+                        font_size: GAMEOVER_FONT_SIZE,
+                        color: TEXT_COLOR,
+                    }
+                }],
+                alignment: TextAlignment {
+                    vertical: VerticalAlign::Center,
+                    horizontal: HorizontalAlign::Center,
+                },
+            },
+            style: Style {
+                align_self: AlignSelf::Center,
+                align_content: AlignContent::Center,
+                align_items: AlignItems::Center,
+                ..default()
+            },
+            ..Default::default()
+        });
 }
